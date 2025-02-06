34775ccdce022082b093e276ecb82788395c8b56
kazyu0 <kazumakimijima.sb@gmail.com>
2025-02-06 19:17:06 +0900

    新規登録エラーの類

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "must include at least one letter and one digit"}
-  validates :password_confirmation, presence: true
-  validate :password_match
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "must include at least one letter and one digit"}
+ 
 
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "は全角文字（漢字・ひらがな・カタカナ）を使用してください" }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "は全角文字（漢字・ひらがな・カタカナ）を使用してください" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナを使用してください" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナを使用してください" }
+  validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナを使用してください" }
+  validates :first_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナを使用してください" }
   validates :birthday, presence: true
   
-  private
 
-  def password_match
-  errors.add(:password_confirmation, "doesn't match Password") if password != password_confirmation
-  end
 end
