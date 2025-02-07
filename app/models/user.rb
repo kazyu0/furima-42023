08164403f3b03c5b7d791d0a9f802c48793390b8
kazyu0 <kazumakimijima.sb@gmail.com>
2025-02-07 17:59:43 +0900

    修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'must include at least one letter and one digit' }
 
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ-龥々ー]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）を使用してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ-龥々ー]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）を使用してください' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ-ン一-龥々ー]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）を使用してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ-ン一-龥々ー]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）を使用してください' }
   validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナを使用してください' }
   validates :first_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナを使用してください' }
   validates :birthday, presence: true
