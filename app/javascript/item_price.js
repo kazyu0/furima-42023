b99cce6f838cf6a46e44b716be1421c614a6029a
kazyu0 <kazumakimijima.sb@gmail.com>
2025-02-09 18:54:16 +0900

    商品出品機能

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,9 @@
-window.addEventListener('turbo:load', () => {
+function price (){
   const priceInput = document.getElementById("item-price");
   const feeDisplay = document.getElementById("add-tax-price");
   const profitDisplay = document.getElementById("profit")
   if (priceInput) {
-    priceInput.addEventListener("input", function() {
+      priceInput.addEventListener("input", function() {
         const price = parseInt(priceInput.value);
         if (isNaN(price) || price <= 0) {
             feeDisplay.textContent = "手数料: -- 円";
@@ -17,8 +17,8 @@ window.addEventListener('turbo:load', () => {
         feeDisplay.textContent = `手数料: ${fee} 円`;
         profitDisplay.textContent = `利益: ${profit} 円`;
     });
-    window.addEventListener("turbo:load", price);
+  }
+};
+window.addEventListener("turbo:load", price);
 window.addEventListener("turbo:render", price);
-}
-});
 
