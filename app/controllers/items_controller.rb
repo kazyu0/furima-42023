23ef16ec4aebaf754adf93e059f650d5f989968f
kazyu0 <kazumakimijima.sb@gmail.com>
2025-02-11 11:00:19 +0900

    商品一覧機能表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items= Item.all
+    @items= Item.order(created_at: :desc)
     if @items.empty?
       @dummy_item = { name: 'ダミー商品', description: 'ここに商品がありません。' }
     end
