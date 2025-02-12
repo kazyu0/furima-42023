7c5a6cb4fefe2f8e73018f8f3ff207681f5e8da8
kazyu0 <kazumakimijima.sb@gmail.com>
2025-02-12 21:46:09 +0900

    情報編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :update]
+  before_action :authorize_user, only: [:edit, :update]
+
 
   def index
     @items= Item.order(created_at: :desc)
@@ -26,11 +29,35 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to @item, notice: '商品情報を更新しました。'
+    else
+      flash[:alert] = @item.errors.full_messages.join(", ")
+      render :edit
+    end
+  end
+
 
 
 private
 
   def item_params
   params.require(:item).permit( :image, :name, :description, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def authorize_user
+    if @item.user_id != current_user.id
+      redirect_to root_path, alert: 'あなたにはこの商品の編集権限がありません。'
+    end
   end
 end
