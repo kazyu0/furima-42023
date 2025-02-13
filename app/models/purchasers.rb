1346264ffc8bfd2cf96840278931d056951e60c8
kazyu0 <kazumakimijima.sb@gmail.com>
2025-02-13 19:08:01 +0900

    購入機能

---
+++ app/models/purchasers.rb
@@ -0,0 +1,8 @@
+class Purchaser < ApplicationRecord
+  extend ActiveHash::Associations ::ActiveRecordExtensions
+ 
+  belongs_to :user
+  belongs_to :item
+  has_one :destination
+  
+end
---
+++ db/migrate/20250213084248_purchaser.rb
@@ -0,0 +1,4 @@
+class Purchaser < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
